32a1ee1c9f4633b455bbee48f948fd463e6fc0ea
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-08-26 22:42:30 +0200

    project init

---
+++ src/main/java/tk/vxmvconverter/app/AppApplication.java
@@ -0,0 +1,12 @@
+package tk.vxmvconverter.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+}
