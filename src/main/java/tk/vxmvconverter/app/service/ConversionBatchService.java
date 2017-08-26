435f1068ad6c8e62644f39afcd809d18875f5a78
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-08-26 23:54:56 +0200

    development

--- src/main/java/tk/vxmvconverter/app/service/ConversionBatchService.java
+++ src/main/java/tk/vxmvconverter/app/service/ConversionBatchService.java
@@ -1,19 +1,51 @@
 package tk.vxmvconverter.app.service;
 
+import org.apache.commons.io.IOUtils;
 import tk.vxmvconverter.app.domain.DestinationVersion;
+import tk.vxmvconverter.app.dto.FileData;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class ConversionBatchService {
 
-    public ByteArrayOutputStream convertSingleImage(ByteArrayInputStream bais, DestinationVersion destinationVersion) {
+    public ByteArrayOutputStream convertSingleImage(ByteArrayInputStream bais, DestinationVersion destinationVersion) throws Exception {
 
         return null;
     }
 
-    public ByteArrayOutputStream convertZipFile(ByteArrayInputStream bais, DestinationVersion destinationVersion) {
+    public ByteArrayOutputStream convertZipFile(ByteArrayInputStream bais, DestinationVersion destinationVersion) throws Exception {
+        List<FileData> fileDataList = new ArrayList<>();
+        ZipInputStream zis = new ZipInputStream(bais);
+        try {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                FileData fileData = new FileData();
+                fileData.setName(entry.getName());
+                fileData.setData(IOUtils.toByteArray(zis));
+                fileDataList.add(fileData);
+            }
+        } finally {
+            zis.close();
+        }
 
-        return null;
+        switch (destinationVersion) {
+            case MV:
+                fileDataList.forEach(fileData -> toMVImage(fileData));
+            case VX:
+                fileDataList.forEach(fileData -> toVXImage(fileData));
+        }
+
+
+    }
+
+    private void toMVImage(FileData fileData) {
+    }
+
+    private void toVXImage(FileData fileData) {
     }
 }
