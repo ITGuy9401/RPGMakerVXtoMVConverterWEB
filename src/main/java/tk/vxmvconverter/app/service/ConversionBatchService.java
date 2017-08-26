89b7718c34325629e46997041a7606d746122823
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-08-27 01:51:14 +0200

    development

--- src/main/java/tk/vxmvconverter/app/service/ConversionBatchService.java
+++ src/main/java/tk/vxmvconverter/app/service/ConversionBatchService.java
@@ -1,10 +1,15 @@
 package tk.vxmvconverter.app.service;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Service;
 import tk.vxmvconverter.app.domain.DestinationVersion;
 import tk.vxmvconverter.app.dto.FileData;
+import tk.vxmvconverter.app.exception.ConverterException;
 import tk.vxmvconverter.app.exception.Error;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -16,6 +21,7 @@ import java.util.zip.ZipOutputStream;
 
 import static tk.vxmvconverter.app.exception.ExceptionUtil.resolveConverterException;
 
+@Service
 public class ConversionBatchService {
 
     public void convertSingleImage(FileData fileData, DestinationVersion destinationVersion) {
@@ -96,8 +102,58 @@ public class ConversionBatchService {
     }
 
     private void toMVImage(FileData fileData) {
+        ByteArrayInputStream in = new ByteArrayInputStream(fileData.getData());
+        try {
+            int width = 0, height = 0;
+            BufferedImage img = ImageIO.read(in);
+
+            height = (img.getHeight() * 150) / 100;
+            width = (img.getWidth() * 150) / 100;
+
+            Image scaledImage = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            BufferedImage imageBuff = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = imageBuff.createGraphics();
+            g2d.setComposite(AlphaComposite.Clear);
+            g2d.fillRect(0, 0, width, height);
+
+            g2d.setComposite(AlphaComposite.Src);
+            g2d.drawImage(scaledImage, 0, 0, null);
+
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+            ImageIO.write(imageBuff, "png", buffer);
+
+            fileData.setData(buffer.toByteArray());
+        } catch (IOException e) {
+            throw new ConverterException(Error.ERROR_CONVERTING_THE_IMAGE, e);
+        }
     }
 
     private void toVXImage(FileData fileData) {
+        ByteArrayInputStream in = new ByteArrayInputStream(fileData.getData());
+        try {
+            int width = 0, height = 0;
+            BufferedImage img = ImageIO.read(in);
+
+            height = (img.getHeight() * 100) / 150;
+            width = (img.getWidth() * 100) / 150;
+
+            Image scaledImage = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            BufferedImage imageBuff = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = imageBuff.createGraphics();
+            g2d.setComposite(AlphaComposite.Clear);
+            g2d.fillRect(0, 0, width, height);
+
+            g2d.setComposite(AlphaComposite.Src);
+            g2d.drawImage(scaledImage, 0, 0, null);
+
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+            ImageIO.write(imageBuff, "png", buffer);
+
+            fileData.setData(buffer.toByteArray());
+        } catch (IOException e) {
+            throw new ConverterException(Error.ERROR_CONVERTING_THE_IMAGE, e);
+        }
     }
 }
